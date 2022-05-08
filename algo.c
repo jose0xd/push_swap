969ee46e44ecf08a12b52ad3b49347f0936e6a16
Jose <direccionjaa@gmail.com>
2022-05-08 08:36:12 +0200

    Order 5 elements in less than 12 actions

--- algo.c
+++ algo.c
@@ -6,7 +6,7 @@
 /*   By: jarredon <jarredon@student.42malaga>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 20:48:00 by jarredon          #+#    #+#             */
-/*   Updated: 2022/05/08 00:02:04 by jarredon         ###   ########.fr       */
+/*   Updated: 2022/05/08 08:33:51 by jarredon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,26 +83,30 @@ void	only_three(t_stack_ptr *stack_a, t_stack_ptr *stack_b, t_orders **orders)
 	}
 }
 
-// TODO 4 2 5 3 1 do 14 actions, too much.
 void	more_than_three(t_stack_ptr *stack_a, t_stack_ptr *stack_b, t_orders **orders)
 {
+	t_node	*max;
+	char	*op;
+
 	while (len_stack(*stack_a) > 3)
 		do_action("pb", stack_a, stack_b, orders);
 	only_three(stack_a, stack_b, orders);
+	max = (*stack_a)->top->prev;
 	while (*stack_b)
 	{
-		if (top(*stack_b) > (*stack_a)->top->prev->n)
+		if (top(*stack_a) > top(*stack_b) && top(*stack_b) > (*stack_a)->top->prev->n)
+			do_action("pa", stack_a, stack_b, orders);
+		else if ((*stack_a)->top->prev == max && top(*stack_b) > max->n)
 		{
 			do_action("pa", stack_a, stack_b, orders);
-			do_action("ra", stack_a, stack_b, orders);
+			max = (*stack_a)->top;
 		}
 		else
-		{
-			while (top(*stack_b) > top(*stack_a))
-				do_action("ra", stack_a, stack_b, orders);
-			do_action("pa", stack_a, stack_b, orders);
-			while (top(*stack_a) > (*stack_a)->top->prev->n)
-				do_action("rra", stack_a, stack_b, orders);
-		}
+			do_action("ra", stack_a, stack_b, orders);
 	}
+	op = "rra";
+	if ((*stack_a)->top == max || (*stack_a)->top->next == max)
+		op = "ra";
+	while (top(*stack_a) > (*stack_a)->top->prev->n)
+		do_action(op, stack_a, stack_b, orders);
 }
